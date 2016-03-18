badc3465ae1dfa06a2e4096bf3ddea15186b6bf8
John Balderson <John Balderson>
2016-03-18 08:25:39 -0400

    Commit from John Balderson

---
+++ app/src/main/java/com/johnbalderson/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.johnbalderson.android.geoquiz;
+
+/**
+ * Created by win10 on 3/15/2016.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
+
